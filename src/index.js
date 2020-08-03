87c9118720d617f9f493b455e86c38b4a86915f6
Tiago <acrossicauda@hotmail.com.br>
2020-08-03 02:53:33 -0300

    commit aula 2 e um pouco da aula 3
    - Telas home e cadastro de categorias finalizados
    - falta ajustar uns bugs na tela de cad categorias
    - carousel feito

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './pages/Home';
+import CadastroVideo from './pages/Video';
+import CadastroCategoria from './pages/Categoria';
+import PageNotFound from './pages/PageNotFound';
+
+import {  BrowserRouter, Switch, Route  } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/cadastro/video" component={CadastroVideo} />
+        <Route path="/cadastro/categoria" component={CadastroCategoria} />
+        <Route path="/" component={App} exact />
+        <Route component={PageNotFound} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
